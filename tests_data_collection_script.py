e62e8263cd082605f5e0deecc22c9ea90a906dee
Vlado Paskov <vladimir@paskov.info>
2018-12-27 16:31:26 +0200

    Adds additional tests for getting API data and adding library to requirements.

--- tests_data_collection_script.py
+++ tests_data_collection_script.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from unittest.mock import Mock
 from unittest.mock import patch
 import json
+import requests_testing
 
 
 class TestCollectionScript(unittest.TestCase):
@@ -68,10 +69,9 @@ class TestCollectionScript(unittest.TestCase):
         out_df = dc.filter_departures(input_data)
         assert(len(out_df) == 0)
 
-    @patch('data_collection_script.requests.get')
-    def tests_get_departures_single_get(self, mock_get):
-
-        mock_data = {        
+    @requests_testing.activate
+    def tests_get_departures_single_get(self):
+        mock_data = {
             "count": 150,
             "next": None,
             "previous": None,
@@ -88,12 +88,69 @@ class TestCollectionScript(unittest.TestCase):
                     "finish_date": "2018-09-10",
                     "category": "Adventurous"
                 }]
-            }
+        }
 
-        mock_get.return_value.content = json.dumps(mock_data)
+        requests_testing.add(
+            request={'url': 'http://127.0.0.1:8000/departures/'},
+            response={'body': json.dumps(mock_data)}
+        )
 
         response = dc.get_departures()
         assert(response == mock_data['results'])
+
+    @requests_testing.activate
+    def tests_get_departures_get_with_next_data(self):
+        mock_data = {
+            "count": 150,
+            "next": "http://127.0.0.1:8000/departures/?limit=50&offset=50",
+            "previous": None,
+            "results": [
+                {
+                    "name": "New Zealand Safari",
+                    "start_date": "2018-04-03",
+                    "finish_date": "2018-04-13",
+                    "category": "Marine"
+                },
+                {
+                    "name": "New Zealand Encompassed",
+                    "start_date": "2018-08-31",
+                    "finish_date": "2018-09-10",
+                    "category": "Adventurous"
+                }]
+        }
+
+        mock_data_next = {
+            "count": 150,
+            "next": None,
+            "previous": None,
+            "results": [{
+                    "name": "Brazil Adventure",
+                    "start_date": "2018-04-03",
+                    "finish_date": "2018-04-13",
+                    "category": "Classic"
+                },
+                {
+                    "name": "Vietnam Encompassed",
+                    "start_date": "2018-04-03",
+                    "finish_date": "2018-04-13",
+                    "category": "Marine"
+                }]
+        }
+
+        requests_testing.add(
+            request={'url': 'http://127.0.0.1:8000/departures/'},
+            response={'body': json.dumps(mock_data)}
+        )
+        requests_testing.add(
+            request={'url':
+                     'http://127.0.0.1:8000/departures/?limit=50&offset=50'},
+            response={'body': json.dumps(mock_data_next)}
+        )
+
+        # mock_get.return_value.content = json.dumps(mock_data)
+
+        response = dc.get_departures()
+        assert(response == mock_data['results'] + mock_data_next['results'])
 
 if __name__ == '__main__':
     unittest.main()
